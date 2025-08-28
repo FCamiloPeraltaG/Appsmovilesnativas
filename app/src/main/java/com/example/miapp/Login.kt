ba91f3e0c292ae0decd3eaefea415c17eb22f471
Camilo1327 <peraltacamilo1@gmail.com>
2025-08-27 22:51:27 -0500

    Primera version, se realiza el splash personalizado, se crea la pantalla inicial y se crea la visual del login

--- app/src/main/java/com/example/miapp/Login.kt
+++ app/src/main/java/com/example/miapp/Login.kt
@@ -1,29 +1,20 @@
 package com.example.miapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-
         }
-        val btnStart = findViewById<Button>(R.id.btn_start)
-        btnStart.setOnClickListener {
-            val intent = Intent(this, Login::class.java)
-            startActivity(intent)
-        }
-
     }
 }
