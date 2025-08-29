aec531ade20b70c497a033f0ad5a4a2257950382
Camilo1327 <peraltacamilo1@gmail.com>
2025-08-28 19:54:08 -0500

    Se arreglan las funciones, se utilizan mejores practicas y se crea la pantalla de recuperar contraseña

--- app/src/main/java/com/example/miapp/HomeActivity.kt
+++ app/src/main/java/com/example/miapp/HomeActivity.kt
@@ -2,27 +2,29 @@ package com.example.miapp
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Login : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.loginMain)) { v, insets ->
+        setContentView(R.layout.activity_home)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        val forgotPassword = findViewById<TextView>(R.id.loginForgotPassword)
 
-        forgotPassword.setOnClickListener {
+        }
+        val btnStart = findViewById<Button>(R.id.homeStartBtn)
+        btnStart.setOnClickListener {
             val intent = Intent(this, Login::class.java)
-            startActivity(intent)        }
+            startActivity(intent)
+
+        }
+
     }
 }
